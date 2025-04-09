2ec1e702e2694264ff05c60d39845afced5e07d7
DSouza25 <souzadario25@gmail.com>
2025-04-09 13:23:02 -0400

    testando commit

--- src/main/java/com/webservices/Webservices/entities/User.java
+++ src/main/java/com/webservices/Webservices/entities/User.java
@@ -13,7 +13,7 @@ import jakarta.persistence.Table;
 public class User implements Serializable{
     private static final long serialVersionUID = 1L;    
 
-    @Id
+    @Ids
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
